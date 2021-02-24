e9a94f17399e0a5e9f6133b0de3a35b42ab4f4ec
yuanyunqiang <649756903@qq.com>
2021-02-24 18:13:10 +0800

    Signed-off-by: yuanyunqiang <649756903@qq.com>

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
 import sys
 import time
 import qdarkstyle
+import os
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from Ui_main import Ui_MainWindow
-from PyQt5.QtCore import QTimer
-import os
+from PyQt5.QtCore import QTimer,QThread
+import requests
+from PyQt5.QtCore import *
+from bs4 import BeautifulSoup
+from lxml import etree
+
+class Runthread(QThread):
+    # python3,pyqt5与之前的版本有些不一样
+    #  通过类成员对象定义信号对象
+    _signal = pyqtSignal(str)
+    def __init__(self):
+        super(Runthread, self).__init__()
+
+    def __del__(self):
+        self.wait()
+ 
+    def run(self):
+        response = requests.get('https://tianqi.qq.com/index.htm')
+        response.encoding = response.apparent_encoding
+        print(response.text)
+        data=self.xpath_parse_movies(response.text)
+        for i in data:
+            print(i)
+
+    def xpath_parse_movies(self,html):
+        et_html = etree.HTML(html)
+        urls = et_html.xpath('//*[@id="txt-temperature"]')
+        print(urls)
+        for index in range(len(urls)):
+            print(urls[index].text)
+        return urls
+
+
+
+
 
 os.environ['QT_API'] = 'pyqt5'
 class MainWindow(QMainWindow,Ui_MainWindow):
@@ -14,6 +48,9 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.timer = QTimer()  # 初始化定时器
         self.timer.timeout.connect(self.time_)
         self.timer.start(100)
+        self.thread = Runthread() # 创建线程
+        #self.thread._signal.connect(self.callbacklog) # 连接信号
+        self.thread.start() # 开始线程
     def time_(self):
         h=time.strftime("%H", time.localtime())
         m=time.strftime("%M", time.localtime())
@@ -22,6 +59,10 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         ow=int(h)*60*60+int(m)*60+int(s)
         self.label.setText(h1+":"+m+":"+s)
         self.progressBar.setValue(ow)
+
+    
+
+
 if __name__ == "__main__":
     App = QApplication(sys.argv)
     ex = MainWindow()
